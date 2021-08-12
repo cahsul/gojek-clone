97ea6892d346901d993ed78a4f8cbcc80e51c9bd
cahsul <cahsul.dev@gmail.com>
2021-08-12 09:12:55 +0700

    news

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,19 +1,21 @@
 import React, {Component} from 'react';
-import {Image, Text, TextInput, View} from 'react-native';
+import {Image, ScrollView, Text, TextInput, View} from 'react-native';
 import Searchbar from './searchbar';
 import BottomNavigation from './bottom-navigation';
 import Gopay from './gopay';
 import MainFeature from './main-feature';
+import News from './news';
 
 class Home extends Component {
   render() {
     return (
       <View style={{flex: 1}}>
-        <View style={{flex: 1, backgroundColor: 'white'}}>
+        <ScrollView style={{flex: 1, backgroundColor: 'white'}}>
           <Searchbar />
           <Gopay />
           <MainFeature />
-        </View>
+          <News />
+        </ScrollView>
         <BottomNavigation />
       </View>
     );
---
+++ src/pages/Home/news.js
@@ -0,0 +1,210 @@
+import React from 'react';
+import {Button, Image, Text, View} from 'react-native';
+
+const News = () => {
+  return (
+    <View style={{marginTop: 20, marginHorizontal: 17}}>
+      <View>
+        <View style={{position: 'relative'}}>
+          <Image
+            style={{height: 200, width: '100%', borderRadius: 10}}
+            source={require('../../assets/images/news/go-food-banka.jpg')}
+          />
+          <Image
+            style={{
+              position: 'absolute',
+              top: 10,
+              left: 10,
+              height: 20,
+              width: 63,
+            }}
+            source={require('../../assets/logos/white.png')}
+          />
+        </View>
+        <View>
+          <Text style={{fontWeight: 'bold', fontSize: 20, marginTop: 10}}>
+            GO-NEWS
+          </Text>
+        </View>
+        <View>
+          <Text style={{fontSize: 14, marginTop: 1, color: '#7a7a7a'}}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+          </Text>
+        </View>
+        <View style={{marginTop: 20}}>
+          <View
+            style={{
+              backgroundColor: '#61a756',
+              paddingHorizontal: 17,
+              paddingVertical: 10,
+              borderRadius: 5,
+              alignSelf: 'flex-end',
+            }}>
+            <Text style={{color: 'white'}}>BACA</Text>
+          </View>
+        </View>
+        <View
+          style={{
+            height: 3,
+            backgroundColor: '#D9E1E3',
+            marginTop: 20,
+            marginBottom: 10,
+          }}
+        />
+      </View>
+
+      <View>
+        <View style={{position: 'relative'}}>
+          <Image
+            style={{height: 200, width: '100%', borderRadius: 10}}
+            source={require('../../assets/images/news/go-food-gm.jpg')}
+          />
+          <Image
+            style={{
+              position: 'absolute',
+              top: 10,
+              left: 10,
+              height: 20,
+              width: 63,
+            }}
+            source={require('../../assets/logos/white.png')}
+          />
+        </View>
+        <View>
+          <Text style={{fontWeight: 'bold', fontSize: 20, marginTop: 10}}>
+            GO-NEWS
+          </Text>
+        </View>
+        <View>
+          <Text style={{fontSize: 14, marginTop: 1, color: '#7a7a7a'}}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+          </Text>
+        </View>
+        <View style={{marginTop: 20}}>
+          <View
+            style={{
+              backgroundColor: '#61a756',
+              paddingHorizontal: 17,
+              paddingVertical: 10,
+              borderRadius: 5,
+              alignSelf: 'flex-end',
+            }}>
+            <Text style={{color: 'white'}}>BACA</Text>
+          </View>
+        </View>
+        <View
+          style={{
+            height: 3,
+            backgroundColor: '#D9E1E3',
+            marginTop: 20,
+            marginBottom: 10,
+          }}
+        />
+      </View>
+
+      <View>
+        <View style={{position: 'relative'}}>
+          <Image
+            style={{height: 200, width: '100%', borderRadius: 10}}
+            source={require('../../assets/images/news/go-food-pak-boss.jpg')}
+          />
+          <Image
+            style={{
+              position: 'absolute',
+              top: 10,
+              left: 10,
+              height: 20,
+              width: 63,
+            }}
+            source={require('../../assets/logos/white.png')}
+          />
+        </View>
+        <View>
+          <Text style={{fontWeight: 'bold', fontSize: 20, marginTop: 10}}>
+            GO-NEWS
+          </Text>
+        </View>
+        <View>
+          <Text style={{fontSize: 14, marginTop: 1, color: '#7a7a7a'}}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+          </Text>
+        </View>
+        <View style={{marginTop: 20}}>
+          <View
+            style={{
+              backgroundColor: '#61a756',
+              paddingHorizontal: 17,
+              paddingVertical: 10,
+              borderRadius: 5,
+              alignSelf: 'flex-end',
+            }}>
+            <Text style={{color: 'white'}}>BACA</Text>
+          </View>
+        </View>
+        <View
+          style={{
+            height: 3,
+            backgroundColor: '#D9E1E3',
+            marginTop: 20,
+            marginBottom: 10,
+          }}
+        />
+      </View>
+
+      <View>
+        <View style={{position: 'relative'}}>
+          <Image
+            style={{height: 200, width: '100%', borderRadius: 10}}
+            source={require('../../assets/images/news/go-food-kfc.jpg')}
+          />
+          <Image
+            style={{
+              position: 'absolute',
+              top: 10,
+              left: 10,
+              height: 20,
+              width: 63,
+            }}
+            source={require('../../assets/logos/white.png')}
+          />
+        </View>
+        <View>
+          <Text style={{fontWeight: 'bold', fontSize: 20, marginTop: 10}}>
+            GO-NEWS
+          </Text>
+        </View>
+        <View>
+          <Text style={{fontSize: 14, marginTop: 1, color: '#7a7a7a'}}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+          </Text>
+        </View>
+        <View style={{marginTop: 20}}>
+          <View
+            style={{
+              backgroundColor: '#61a756',
+              paddingHorizontal: 17,
+              paddingVertical: 10,
+              borderRadius: 5,
+              alignSelf: 'flex-end',
+            }}>
+            <Text style={{color: 'white'}}>BACA</Text>
+          </View>
+        </View>
+        <View
+          style={{
+            height: 3,
+            backgroundColor: '#D9E1E3',
+            marginTop: 20,
+            marginBottom: 10,
+          }}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default News;
